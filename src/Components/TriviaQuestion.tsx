2dec40641d8a920bef31562abe221530dc19b8ea
niks200 <nikhilkalariya1000@gmail.com>
2024-06-26 22:25:22 +0530

    design changes

--- src/Components/TriviaQuestion.tsx
+++ src/Components/TriviaQuestion.tsx
@@ -108,7 +108,7 @@ const TriviaQuestion = () => {
                     ))}
                 </form>
             </div>
-            <div className="flex items-center justify-between">
+            <div className="flex items-center justify-between m-5">
                 <button className="text-white bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2" onClick={handleSubmit}>
                     Submit
                 </button>
